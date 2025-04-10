ef9abb2c43f3cb810cec1fb4a131a88af73b4d6d
gliese-backup <glieseb+1@proton.me>
2025-04-10 14:40:58 +0000

    add tailwind and daisyui

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,9 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function Page() {
   return (
     <div>
-      <h3>Hello world</h3>
+      <h3 className="text-amber-400">Hello world</h3>
       <Link href="/login">Go to login page</Link>
     </div>
   );
