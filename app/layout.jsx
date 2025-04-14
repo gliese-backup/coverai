9f316fb5fb62f2abdd7292c878d28bc65d98de69
gliese-backup <glieseb+1@proton.me>
2025-04-14 14:21:44 +0000

    Add header + footer + alias for components

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,7 @@
 import "./global.css";
-import Link from "next/link";
+
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 export const metadata = {
   title: "Next.js",
@@ -8,27 +10,15 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html
+      lang="en"
+      data-theme="business"
+    >
       <body>
         {/* Header */}
-        <div className="navbar bg-base-100 shadow-sm">
-          <div className="flex-1">
-            <Link
-              href="/"
-              className="btn btn-ghost text-xl"
-            >
-              CoverAI
-            </Link>
-          </div>
-          <div className="flex-none">
-            <ul className="menu menu-horizontal px-1">
-              <li>
-                <Link href="/login">Login</Link>
-              </li>
-            </ul>
-          </div>
-        </div>
+        <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Page() {
   return (
-    <div>
+    <div className="min-h-screen">
       <h3 className="text-amber-400">Hello world</h3>
       <Link href="/login">Go to login page</Link>
     </div>
